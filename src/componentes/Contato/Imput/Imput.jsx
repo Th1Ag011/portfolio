2e7e10c55170d09961eb9c21551e9ace04668d29
ThiagoVM11 <thiagovm016@gmail.com>
2024-02-02 16:04:04 -0300

    edit inputs

--- src/componentes/Contato/Imput/Imput.jsx
+++ src/componentes/Contato/Imput/Imput.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Imput = ({ type, placeholder, name }) => {
   return (
-    <div>
-      <input required placeholder={placeholder} type={type} name={name} 
-      className="bg-[#312e2e] font-mono text-gray-500 dark:bg-[#cecdcd] placeholder:text-gray-400 focus:ring-2 sm:w-[400px] w-[300px] my-2 h-[50px] rounded-md px-4"
-      />
-    </div>
+<input
+  required
+  placeholder={placeholder}
+  type={type}
+  name={name}
+  className="resize-none sm:w-[400px] w-[300px] my-2 h-[50px]  bg-[#312e2e] dark:bg-[#cecdcd] rounded-lg placeholder:text-gray-400 text-gray-50 dark:placeholder:text-gray-400 dark:text-gray-700 p-4 focus:outline-none focus:ring-2 ring-[#201f1f]"
+/>
   );
 };
 
